8a346bfad0774cd1f27b24e2f265f624333a037c
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-01 16:40:08 +0530

    Updated

--- Section 1/JOptionPane/src/jOptionPane/jOption.java
+++ Section 1/JOptionPane/src/jOptionPane/jOption.java
@@ -9,5 +9,18 @@ public class jOption {
         name= JOptionPane.showInputDialog("Please enter you name: ");
         surname=JOptionPane.showInputDialog("Please enter your surname");
         JOptionPane.showMessageDialog(null,name+" "+surname);
+
+        int firstNumber;
+        int secondNumber;
+        String input;
+        input=JOptionPane.showInputDialog("Please enter first number");
+        firstNumber=Integer.parseInt(input);
+        input=JOptionPane.showInputDialog("please enter second number");
+        secondNumber=Integer.parseInt(input);
+
+        double average;
+        average=(firstNumber+secondNumber)/2.0;
+        JOptionPane.showMessageDialog(null,"Average" +" = "+average);
+        System.out.println(average);
     }
 }
