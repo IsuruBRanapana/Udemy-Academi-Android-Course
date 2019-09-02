55e2de3ae6d86e75c61f4d1469e9e05feaf11ce5
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-02 21:31:33 +0530

    User controlled loop

--- Section 1/Input Validation/src/com/example/calculation/UserControlledLoop.java
+++ Section 1/Input Validation/src/com/example/calculation/UserControlledLoop.java
@@ -1,4 +1,16 @@
 package com.example.calculation;
 
+import javax.swing.*;
+
 public class UserControlledLoop {
+    public static void main(String[] args) {
+        int maxValue;
+        String input= JOptionPane.showInputDialog("How High should I go to square the number");
+        maxValue=Integer.parseInt(input);
+        System.out.println("Number          Number Squared");
+        System.out.println("-----------------------------------");
+        for (int number=1;number<=maxValue;number++){
+            System.out.println(number+"\t\t\t\t\t"+number*number);
+        }
+    }
 }
