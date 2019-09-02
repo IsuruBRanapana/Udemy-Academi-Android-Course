dd0b162514f5e09c665176801f97165d15313736
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-02 22:49:13 +0530

    Sentine Values

--- Section 1/Input Validation/src/com/example/calculation/RunningTotals.java
+++ Section 1/Input Validation/src/com/example/calculation/RunningTotals.java
@@ -1,12 +1,19 @@
 package com.example.calculation;
 
 import javax.swing.*;
+import java.text.DecimalFormat;
 
 public class RunningTotals {
     public static void main(String[] args) {
         int days;
         double sales;
         double totalSales=0.0; //running total
+        DecimalFormat dollar=new DecimalFormat("#,##0.00");//# are represent non-required once.
+        /**
+         * # means non-required once. it means there can be value or not.
+         * in 0 positions if there are nothing, programme automatically put 0
+         * in # positions if there are nothing, programme don't put anything
+         */
 
         String input = JOptionPane.showInputDialog("For how many days do you have sales"+"Figures?");
         days=Integer.parseInt(input);
@@ -15,7 +22,7 @@ public class RunningTotals {
             sales=Double.parseDouble(input);
             totalSales += sales;
         }
-        JOptionPane.showMessageDialog(null,"The total sales are $"+totalSales);
+        JOptionPane.showMessageDialog(null,"The total sales are $"+dollar.format(totalSales));
         System.exit(0);
 
     }
--- Section 1/Input Validation/src/com/example/calculation/SentinelValues.java
+++ Section 1/Input Validation/src/com/example/calculation/SentinelValues.java
@@ -1,4 +1,19 @@
 package com.example.calculation;
 
+import javax.swing.*;
+
 public class SentinelValues {
+    public static void main(String[] args) {
+        int value;
+        int doubleValue;
+
+        String input= JOptionPane.showInputDialog("Please enter a value to double"+"(0 To Stop)");
+        value=Integer.parseInt(input);
+        while (value!=0){
+            doubleValue=value*2;
+            JOptionPane.showMessageDialog(null," "+value+" doubled is"+doubleValue);
+            input= JOptionPane.showInputDialog("Please enter a value to double"+"(0 To Stop)");
+            value=Integer.parseInt(input);
+        }
+    }
 }
