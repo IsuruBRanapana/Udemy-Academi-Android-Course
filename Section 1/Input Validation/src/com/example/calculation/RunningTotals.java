cd686f5073475a3b47ef08c0368f63fe9b6b797b
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-02 22:35:20 +0530

    Running Totals

--- Section 1/Input Validation/src/com/example/calculation/RunningTotals.java
+++ Section 1/Input Validation/src/com/example/calculation/RunningTotals.java
@@ -1,4 +1,22 @@
 package com.example.calculation;
 
+import javax.swing.*;
+
 public class RunningTotals {
+    public static void main(String[] args) {
+        int days;
+        double sales;
+        double totalSales=0.0; //running total
+
+        String input = JOptionPane.showInputDialog("For how many days do you have sales"+"Figures?");
+        days=Integer.parseInt(input);
+        for (int count=1;count<=days;count++){
+            input=JOptionPane.showInputDialog("Enter the sales for day "+count);
+            sales=Double.parseDouble(input);
+            totalSales += sales;
+        }
+        JOptionPane.showMessageDialog(null,"The total sales are $"+totalSales);
+        System.exit(0);
+
+    }
 }
