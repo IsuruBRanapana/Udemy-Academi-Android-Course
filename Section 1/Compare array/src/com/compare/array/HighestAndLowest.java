31f85892b4e3301b369d02f80125e5049db6656a
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-03 21:42:23 +0530

    Updated

--- Section 1/Compare array/src/com/compare/array/HighestAndLowest.java
+++ Section 1/Compare array/src/com/compare/array/HighestAndLowest.java
@@ -4,8 +4,9 @@ public class HighestAndLowest {
     public static void main(String[] args) {
         int [] numbers={2,4,6,8,10,15,23,761,12,1};
         int highest=HighestValue(numbers);
-        System.out.println(highest);
-
+        System.out.println("The Highest value is "+highest);
+        int lowest = LowestValue(numbers);
+        System.out.println("The lowest value is "+lowest);
     }
 
     public static int HighestValue(int [] array){
@@ -16,5 +17,14 @@ public class HighestAndLowest {
             }
         }
         return highest;
+    }
+    public static int LowestValue(int [] array){
+        int lowest=array[0];
+        for (int i=1;i<array.length;i++){
+            if (array[i]<lowest){
+                lowest=array[i];
+            }
+        }
+        return lowest;
     }
 }
--- Section 1/Compare array/src/com/compare/array/SumAndAverage.java
+++ Section 1/Compare array/src/com/compare/array/SumAndAverage.java
@@ -1,4 +1,20 @@
 package com.compare.array;
 
 public class SumAndAverage {
+    public static void main(String[] args) {
+        int [] values={1,2,4,6,8,19,12,75};
+        int sum=Sum(values);
+
+        double average=sum/(double) values.length;
+        System.out.println("the sum is "+sum);
+        System.out.println("the average is "+average);
+    }
+
+    public static int Sum(int [] array){
+        int sum=0;
+        for (int i=0;i<array.length;i++){
+            sum += array[i];
+        }
+        return sum;
+    }
 }
