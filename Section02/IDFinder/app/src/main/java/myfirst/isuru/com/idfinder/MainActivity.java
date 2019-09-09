576e9aa7660bfd03f64850ac1c595fceeb896998
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-09 22:02:26 +0530

    Updated

--- Section02/IDFinder/app/src/main/java/myfirst/isuru/com/idfinder/MainActivity.java
+++ Section02/IDFinder/app/src/main/java/myfirst/isuru/com/idfinder/MainActivity.java
@@ -2,12 +2,30 @@ package myfirst.isuru.com.idfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etId;
+    Button btnSubmit;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etId=findViewById(R.id.etId);
+        btnSubmit=findViewById(R.id.btnSubmit);
+        tvResult=findViewById(R.id.tvResult);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String idNumber=etId.getText().toString().trim();
+            }
+        });
     }
 }
