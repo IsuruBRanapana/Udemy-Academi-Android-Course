444114964a9a7ef09b8499d3ebba27e502e9123e
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-12 13:04:48 +0530

    updated

--- Section03/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/CreateContact.java
+++ Section03/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/CreateContact.java
@@ -1,10 +1,12 @@
 package myfirst.isuru.com.contactbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class CreateContact extends AppCompatActivity {
     EditText etName,etNumber,etWebSite,etLocation;
@@ -38,7 +40,27 @@ public class CreateContact extends AppCompatActivity {
         ivAngry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                if (etName.getText().toString().isEmpty()){
+                    Toast.makeText(CreateContact.this,"Please Enter Name",Toast.LENGTH_SHORT).show();
+                }else if(etNumber.getText().toString().isEmpty()){
+                    Toast.makeText(CreateContact.this,"Please Enter Number",Toast.LENGTH_SHORT).show();
+                }else if (etWebSite.getText().toString().isEmpty()){
+                    Toast.makeText(CreateContact.this,"Please Enter Website",Toast.LENGTH_SHORT).show();
+                }else if (etLocation.getText().toString().isEmpty()){
+                    Toast.makeText(CreateContact.this,"Please Enter Location",Toast.LENGTH_SHORT).show();
+                }else{
+                    String name=etName.getText().toString().trim();
+                    String number=etNumber.getText().toString().trim();
+                    String webSite=etWebSite.getText().toString().trim();
+                    String location=etLocation.getText().toString().trim();
+                    Intent intent=new Intent();
+                    intent.putExtra("name",name);
+                    intent.putExtra("number",number);
+                    intent.putExtra("webSite",webSite);
+                    intent.putExtra("location",location);
+                    setResult(RESULT_OK,intent);
+                    CreateContact.this.finish();
+                }
             }
         });
     }
--- Section03/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/MainActivity.java
+++ Section03/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/MainActivity.java
@@ -1,5 +1,8 @@
 package myfirst.isuru.com.contactbook;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvHeading;
     ImageView ivCall,ivAngryFace,ivWebSite,ivLocation;
     Button btnNewContact;
+    final int CREATECONTACT=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         ivCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String number=getIntent().getStringExtra("number");
+                Intent intent=new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+number));
+                startActivity(intent);
             }
         });
         ivWebSite.setOnClickListener(new View.OnClickListener() {
@@ -49,8 +55,26 @@ public class MainActivity extends AppCompatActivity {
         btnNewContact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent=new Intent(MainActivity.this,myfirst.isuru.com.contactbook.CreateContact.class);
+                startActivityForResult(intent,CREATECONTACT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==CREATECONTACT){
+            if (resultCode==RESULT_OK){
+                ivCall.setVisibility(View.VISIBLE);
+                ivAngryFace.setVisibility(View.VISIBLE);
+                ivLocation.setVisibility(View.VISIBLE);
+                ivWebSite.setVisibility(View.VISIBLE);
+                String name=getIntent().getStringExtra("name");
+                String number=getIntent().getStringExtra("number");
+                String webSite=getIntent().getStringExtra("webSite");
+                String location=getIntent().getStringExtra("location");
+            }
+        }
     }
 }
