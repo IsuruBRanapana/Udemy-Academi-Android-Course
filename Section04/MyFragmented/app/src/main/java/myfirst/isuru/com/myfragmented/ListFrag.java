092ea8dce94ffe9fe0c28b39d7a83790b326719f
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-13 10:27:17 +0530

    updated

--- Section04/MyFragmented/app/src/main/java/myfirst/isuru/com/myfragmented/ListFrag.java
+++ Section04/MyFragmented/app/src/main/java/myfirst/isuru/com/myfragmented/ListFrag.java
@@ -44,7 +44,10 @@ public class ListFrag extends ListFragment {
         String[] data=getResources().getStringArray(R.array.pieces);
 
         setListAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, data));
-        activity.onItemSelected(0);
+        if (this.getActivity().findViewById(R.id.layout_land)!=null){
+            activity.onItemSelected(0);
+        }
+
     }
 
     @Override
--- Section04/MyFragmented/app/src/main/java/myfirst/isuru/com/myfragmented/MainActivity.java
+++ Section04/MyFragmented/app/src/main/java/myfirst/isuru/com/myfragmented/MainActivity.java
@@ -1,6 +1,7 @@
 package myfirst.isuru.com.myfragmented;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,9 +19,36 @@ public class MainActivity extends AppCompatActivity implements ListFrag.ItemSele
 
         descriptions=getResources().getStringArray(R.array.descriptions);
 
+        //the phone is in portrait mode
+        if (findViewById(R.id.layout_portrait)!=null){
+            FragmentManager manager=this.getSupportFragmentManager();
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .hide(manager.findFragmentById(R.id.detailFrag))
+                    .commit();
+
+        }
+        //the phone is in landscape mode
+        if (findViewById(R.id.layout_land)!=null){
+            FragmentManager manager=this.getSupportFragmentManager();
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.listFrag))
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .commit();
+
+        }
+
     }
     @Override
     public void onItemSelected(int index){
         tvDescription.setText(descriptions[index]);
+        if (findViewById(R.id.layout_portrait)!=null){
+            FragmentManager manager=this.getSupportFragmentManager();
+            manager.beginTransaction()
+                    .show(manager.findFragmentById(R.id.detailFrag))
+                    .hide(manager.findFragmentById(R.id.listFrag))
+                    .addToBackStack(null)
+                    .commit();
+        }
     }
 }
