d69f26205cd85b406a75ffad6429aafdfe1139ed
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-14 12:30:55 +0530

    Updated

--- Section04/MyRecyclerView/app/src/main/java/myfirst/isuru/com/myrecyclerview/MainActivity.java
+++ Section04/MyRecyclerView/app/src/main/java/myfirst/isuru/com/myrecyclerview/MainActivity.java
@@ -1,18 +1,48 @@
 package myfirst.isuru.com.myrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     RecyclerView.Adapter myAdapter;
     RecyclerView.LayoutManager myLayoutManager;
+
+    ArrayList<Person> people;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView=findViewById(R.id.list);
+        recyclerView.setHasFixedSize(true);
+
+        myLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(myLayoutManager);
+
+        people=new ArrayList<Person>();
+        people.add(new Person("John","Rambo","bus"));
+        people.add(new Person("John1","Rambo","plane"));
+        people.add(new Person("John2","Rambo","bus"));
+        people.add(new Person("John3","Rambo","bus"));
+        people.add(new Person("John4","Rambo","plane"));
+        people.add(new Person("John","Rambo","bus"));
+        people.add(new Person("John1","Rambo","plane"));
+        people.add(new Person("John2","Rambo","bus"));
+        people.add(new Person("John3","Rambo","bus"));
+        people.add(new Person("John4","Rambo","plane"));
+        people.add(new Person("John","Rambo","bus"));
+        people.add(new Person("John1","Rambo","plane"));
+        people.add(new Person("John2","Rambo","bus"));
+        people.add(new Person("John3","Rambo","bus"));
+        people.add(new Person("John4","Rambo","plane"));
+
+        myAdapter=new PersonAdapter(this,people);
+        recyclerView.setAdapter(myAdapter);
     }
 }
