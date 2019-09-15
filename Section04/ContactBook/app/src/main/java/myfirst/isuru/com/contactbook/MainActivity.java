7e875de8af283d4c526ef13e17ee332f66cb87bb
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-15 10:13:45 +0530

    updated

--- Section04/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/MainActivity.java
+++ Section04/ContactBook/app/src/main/java/myfirst/isuru/com/contactbook/MainActivity.java
@@ -3,12 +3,32 @@ package myfirst.isuru.com.contactbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvName,tvTel;
+    EditText etName,etTel;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvName=findViewById(R.id.tvName);
+        tvTel=findViewById(R.id.tvTel);
+        etName=findViewById(R.id.etName);
+        etTel=findViewById(R.id.etTel);
+        btnAdd=findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
