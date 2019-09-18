60f8d3da5fac567b8ebf5d5cc09fad760fce188c
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-18 16:43:55 +0530

    Updated

---
+++ Section05/SharedPreferencesTest/app/src/main/java/myfirst/isuru/com/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,31 @@
+package myfirst.isuru.com.sharedpreferencestest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvWelcome;
+    EditText etName;
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvWelcome=findViewById(R.id.tvWelcome);
+        etName=findViewById(R.id.etName);
+        btnSubmit=findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name=etName.getText().toString().trim();
+                tvWelcome.setText("Welcome to my app "+name);
+            }
+        });
+    }
+}
