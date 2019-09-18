5e9e53069326230f46f97aea63e99e6d9180149b
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-18 17:04:36 +0530

    Updated

--- Section05/SharedPreferencesTest/app/src/main/java/myfirst/isuru/com/sharedpreferencestest/MainActivity.java
+++ Section05/SharedPreferencesTest/app/src/main/java/myfirst/isuru/com/sharedpreferencestest/MainActivity.java
@@ -2,6 +2,7 @@ package myfirst.isuru.com.sharedpreferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView tvWelcome;
     EditText etName;
     Button btnSubmit;
+    public static final String MY_PREFS_FILE="myfirst.isuru.com.sharedpreferencestest.names";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +22,19 @@ public class MainActivity extends AppCompatActivity {
         tvWelcome=findViewById(R.id.tvWelcome);
         etName=findViewById(R.id.etName);
         btnSubmit=findViewById(R.id.btnSubmit);
+
+        SharedPreferences pref=getSharedPreferences(MY_PREFS_FILE,MODE_PRIVATE);
+        String user=pref.getString("user","");
+        tvWelcome.setText("Welcome to my app "+user);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name=etName.getText().toString().trim();
                 tvWelcome.setText("Welcome to my app "+name);
+
+                SharedPreferences.Editor editor=getSharedPreferences(MY_PREFS_FILE,MODE_PRIVATE).edit();
+                editor.putString("user",name);
+                editor.commit();
             }
         });
     }
