12eb80ae3da4004bb6d3907dd0e05ace88b6ef25
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-25 21:18:54 +0530

    Updated

---
+++ Section08/Contact2018/app/src/main/java/com/android/contact2018/ContactList.java
@@ -0,0 +1,14 @@
+package com.android.contact2018;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
--- Section08/Contact2018/app/src/main/java/com/android/contact2018/MainActivity.java
+++ Section08/Contact2018/app/src/main/java/com/android/contact2018/MainActivity.java
@@ -2,13 +2,31 @@ package com.android.contact2018;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnList,btnCreateNew;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnList=findViewById(R.id.btnList);
+        btnCreateNew=findViewById(R.id.btnCreateNew);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,ContactList.class));
+            }
+        });
+        btnCreateNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,NewContact.class));
+            }
+        });
     }
 }
