d1a9121241c7394f8b747fc8b632689f65773ab9
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-09-25 11:53:25 +0530

    Updated

---
+++ Section08/Contact2018/app/src/main/java/com/android/contact2018/ApplicationClass.java
@@ -0,0 +1,21 @@
+package com.android.contact2018;
+
+import android.app.Application;
+
+import com.backendless.Backendless;
+
+public class ApplicationClass extends Application {
+    public static final String APPLICATION_ID = "E47985CA-5E5F-FC1B-FFB2-467080055400";
+    public static final String API_KEY = "5605C574-8F84-B303-FF18-8EF844967500";
+    public static final String SERVER_URL = "https://api.backendless.com";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Backendless.setUrl( SERVER_URL );
+        Backendless.initApp( getApplicationContext(),
+                APPLICATION_ID,
+                API_KEY );
+    }
+
+}
